0b7b892acee03429ac463a76c5ac7374ae4e5396
Vicente Reis Silva <vicentereissilva@ua.pt>
2024-08-24 23:00:23 +0100

    tutorial mod

---
+++ src/main/java/net/vicente/tutorialmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.vicente.tutorialmod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.vicente.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    // VALUABLES
+    public static final RegistryObject<Item> ALEXANDRITE = ITEMS.register("alexandrite",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> RAW_ALEXANDRITE = ITEMS.register("raw_alexandrite",
+            () -> new Item(new Item.Properties()));
+
+    // TOOLS
+    public static final RegistryObject<Item> ALEXANDRITE_CHISEL = ITEMS.register("alexandrite_chisel",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
